3f712ac1d539c9696ed385f9d812aa9eaee3455e
Robert Schwarz <hello@robertschwarz.io>
2026-09-20 09:15:12 +0200

    refactor: guard inventory patch with ModernUI and ModernCharacterSheet

--- Harmony Patches/Patch_XRL_UI_InventoryScreen.cs
+++ Harmony Patches/Patch_XRL_UI_InventoryScreen.cs
@@ -13,7 +13,7 @@ namespace QudUX.HarmonyPatches
         static bool Prefix(XRL.World.GameObject GO, ref XRL.UI.ScreenReturn __result)
         {
             if (QudUXOptions.UI.UseQudUXInventory
-                && !GameOptions.OverlayPrereleaseInventory
+                && !(GameOptions.ModernUI && GameOptions.ModernCharacterSheet)
                 && XRLCore.Core.Game.Player.Body.GetConfusion() <= 0)
             {
                 __result = new XRL.UI.QudUX_InventoryScreen().Show(GO);
